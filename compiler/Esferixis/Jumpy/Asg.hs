f7f1c3b05f085997e348f7dc12dd2cdc8cc1fcdb
Ariel Favio Carrizo <arifacarr@gmail.com>
2019-05-16 01:51:58 -0300

    Calling conventions, state conventions, var decl, fun decl, instr. scope

--- compiler/Esferixis/Jumpy/Asg.hs
+++ compiler/Esferixis/Jumpy/Asg.hs
@@ -36,17 +36,46 @@ data Entity e =
    StateEntity (StateId e) |
    VarEntity (VarId e)
 
+data LinkageType =
+   PrivateLinkage |
+   InternalLinkage |
+   ExternalLinkage
+
+data MemberAccess =
+   PublicMember |
+   PrivateMember
+
 data EntityName = GlobalEntityName String | LocalEntityName String
 
+data ModuleMemberDecl e ni o where
+   ModuleFunDecl :: Maybe (TaggedNode ni LinkageType) -> FunDecl e ni o -> ModuleMemberDecl e ni o
+   ModuleStateDecl :: Maybe (TaggedNode ni LinkageType) -> StateDecl e ni o -> ModuleMemberDecl e ni o
+
+data ModuleCmd e ni o where
+   ModuleCmdMemberDecl :: (TaggedNode ni MemberAccess) -> ModuleMemberDecl e ni o -> ModuleCmd e ni o
+
+data InstrScopeCmd e ni o where
+   InstrScopeFunDecl :: FunDecl e ni o -> InstrScopeCmd e ni o
+   InstrScopeStateDecl :: StateDecl e ni o -> InstrScopeCmd e ni o
+
 data MaybeInstrScope e ni o where
-   JustInstrScope :: Asg e ni o -> MaybeInstrScope e ni o
+   JustInstrScope :: Asg e ni (InstrScopeCmd e ni) (ni, o) -> MaybeInstrScope e ni o
    NothingInstrScope :: MaybeInstrScope e ni ()
 
-data EntityDecl e ni o =
-   ModuleDecl String ( Asg e ni o ) |
-   FunDecl String ( MaybeInstrScope e ni o ) -- Incomplete
+data TypedArgumentDecl e = TypedArgumentDecl String (TypeId e)
 
-data Asg e ni o where
-   RootModule :: (ModuleId e -> Asg e ni o) -> Asg e ni o
-   FindEntityByName :: EntityName -> (Maybe (TaggedNode ni (Entity e)) -> Asg e ni o) -> Asg e ni o
-   DeclareEntity :: TaggedNode ni (EntityDecl e ni o) -> Asg e ni o
+data ModuleDecl e ni o = ModuleDecl (TaggedNode ni String) ( Asg e ni (ModuleCmd e ni) o)
+
+data CallingConv = CDeclCallingConv | StdCallCallingConv
+data StateConv = CDeclStateConv
+
+data FunDecl e ni o = FunDecl (TaggedNode ni String) (Maybe (TaggedNode ni CallingConv)) [TaggedNode ni (TypedArgumentDecl e)] (MaybeInstrScope e ni o)
+
+data StateContextDecl e ni = StateContextDecl (Maybe (TaggedNode ni String)) (TaggedNode ni (TypeId e))
+data StateDecl e ni o = StateDecl (TaggedNode ni String) (Maybe (TaggedNode ni StateConv)) (TaggedNode ni (StateContextDecl ni e)) (MaybeInstrScope e ni o)
+data VarDecl e ni o = VarDecl (TaggedNode ni String) (TaggedNode ni (TypeId e))
+
+data Asg e ni cmd o where
+   RootModule :: (ModuleId e -> Asg e ni cmd o) -> Asg e ni cmd o
+   FindEntityByName :: EntityName -> (Maybe (TaggedNode ni (Entity e)) -> Asg e ni cmd o) -> Asg e ni cmd o
+   ExecuteCommand :: cmd o -> Asg e ni cmd o
