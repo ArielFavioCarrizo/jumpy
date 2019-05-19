440d8de6fcfd3d049bf4fabfdb7f438e3548bb88
Ariel Favio Carrizo <arifacarr@gmail.com>
2019-05-19 16:00:37 -0300

    Rank-2 type on instruction scope declaration

--- compiler/Esferixis/Jumpy/Asg.hs
+++ compiler/Esferixis/Jumpy/Asg.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE GADTs #-}
 
 module Esferixis.Jumpy.Asg where
@@ -15,6 +16,7 @@ module Esferixis.Jumpy.Asg where
 import Text.Show
 import Data.Word
 import Data.Maybe
+import Data.Either
 import Data.Int
 
 class (Show ni) => JLocInfo ni
@@ -47,37 +49,57 @@ data MemberAccess =
 
 data EntityName = GlobalEntityName String | LocalEntityName String
 
+data FunDeclException x =
+   FunExistsException (FunId x) |
+   FunSignatureMismatch (FunId x)
+   
+data StateDeclException x =
+   StateExistsException (StateId x) |
+   StateTypeMismatch (StateId x)
+   
+data LinkageException x =
+   LinkageHasBeenDeclared (FunId x)
+   
+data LinkedDeclException e =
+   JustLinkageException (LinkageException e ) |
+   OtherException e
+   
+data EntityNotFoundException = EntityNotFoundException EntityName
+
 data ModuleMemberDecl x ni e o where
-   ModuleFunDecl :: Maybe (TaggedNode ni LinkageType) -> FunDecl x ni e o -> ModuleMemberDecl x ni e o
-   ModuleStateDecl :: Maybe (TaggedNode ni LinkageType) -> StateDecl x ni e o -> ModuleMemberDecl x ni e o
+   ModuleFunDecl :: Maybe (TaggedNode ni LinkageType) -> FunDecl x ni e o -> ModuleMemberDecl x ni ( LinkedDeclException ( Either ( FunDeclException x ) e ) ) o
+   ModuleStateDecl :: Maybe (TaggedNode ni LinkageType) -> StateDecl x ni e o -> ModuleMemberDecl x ni ( LinkedDeclException ( Either ( StateDeclException x ) e ) ) o
 
 data ModuleCmd x ni e o where
    ModuleCmdMemberDecl :: (TaggedNode ni MemberAccess) -> ModuleMemberDecl x ni e o -> ModuleCmd x ni e o
 
 data InstrScopeCmd x ni e o where
-   InstrScopeFunDecl :: FunDecl x ni e o -> InstrScopeCmd x ni e o
-   InstrScopeStateDecl :: StateDecl x ni e o -> InstrScopeCmd x ni e o
+   InstrScopeFunDecl :: FunDecl x ni e o -> InstrScopeCmd x ni ( Either (FunDeclException x) e ) o
+   InstrScopeStateDecl :: StateDecl x ni e o -> InstrScopeCmd x ni ( Either (FunDeclException x) e ) o
+   
+data InstrScopeDecl ni e o = InstrScopeDecl ( forall x. Asg x ni (InstrScopeCmd x ni) e (ni, o) ) -- It is rank-2 type because local declarations are only valid in this scope
 
-data MaybeInstrScope x ni e o where
-   JustInstrScope :: Asg x ni (InstrScopeCmd x ni) e (ni, o) -> MaybeInstrScope x ni e o
-   NothingInstrScope :: MaybeInstrScope x ni e ()
+data MaybeInstrScope ni e o where
+   JustInstrScope :: InstrScopeDecl ni e o -> MaybeInstrScope ni e o
+   NothingInstrScope :: MaybeInstrScope ni e ()
 
-data TypedArgumentDecl e = TypedArgumentDecl String (TypeId e)
+data TypedArgumentDecl x = TypedArgumentDecl String (TypeId x)
 
 data ModuleDecl x ni e o = ModuleDecl (TaggedNode ni String) ( Asg x ni (ModuleCmd x ni) e o)
 
 data CallingConv = CDeclCallingConv | StdCallCallingConv
 data StateConv = CDeclStateConv
 
-data FunDecl x ni e o = FunDecl (TaggedNode ni String) (Maybe (TaggedNode ni CallingConv)) [TaggedNode ni (TypedArgumentDecl x)] (MaybeInstrScope x ni e o)
+data FunDecl x ni e o = FunDecl (TaggedNode ni String) (Maybe (TaggedNode ni CallingConv)) [TaggedNode ni (TypedArgumentDecl x)] (MaybeInstrScope ni e o)
 
 data StateContextDecl x ni = StateContextDecl (Maybe (TaggedNode ni String)) (TaggedNode ni (TypeId x))
-data StateDecl x ni e o = StateDecl (TaggedNode ni String) (Maybe (TaggedNode ni StateConv)) (TaggedNode ni (StateContextDecl x ni)) (MaybeInstrScope x ni e o)
+data StateDecl x ni e o = StateDecl (TaggedNode ni String) (Maybe (TaggedNode ni StateConv)) (TaggedNode ni (StateContextDecl x ni)) (MaybeInstrScope ni e o)
 data VarDecl x ni e o = VarDecl (TaggedNode ni String) (TaggedNode ni (TypeId x))
 
 data Asg x ni cmd e o where
-   RootModule :: (ModuleId x -> Asg x ni cmd e o) -> Asg x ni cmd e o
-   FindEntityByName :: EntityName -> (Maybe (TaggedNode ni (Entity x)) -> Asg x ni cmd e o) -> Asg x ni cmd e o
-   Throw :: e -> Asg x ni cmd e o
-   Try :: Asg x ni cmd e o -> Asg x ni cmd e2 (Either e o)
-   ExecuteCommand :: cmd e o -> Asg x ni cmd e o
+   AsgFindEntityByName :: EntityName -> Asg x ni cmd EntityNotFoundException (Entity x)
+   AsgThrow :: e -> Asg x ni cmd e o
+   AsgTry :: Asg x ni cmd e o -> Asg x ni cmd e2 (Either e o)
+   AsgExecuteCommand :: cmd e o -> Asg x ni cmd e o
+   AsgBind :: Asg x ni cmd e i -> ( i -> Asg x ni cmd e o ) -> Asg x ni cmd e o
+   AsgReturn :: o -> Asg x ni cmd e o
